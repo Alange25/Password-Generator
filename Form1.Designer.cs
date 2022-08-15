2fdbf5c3b050a7a182a4cbf178eecd789dec1cb2
Alexander <72223927+Alange25@users.noreply.github.com>
2022-08-15 10:18:26 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace _PasswordGenerator_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.Passwordbtn = new System.Windows.Forms.Button();
+            this.Clearbtn = new System.Windows.Forms.Button();
+            this.Exitbtn = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.CheckBox();
+            this.ten = new System.Windows.Forms.CheckBox();
+            this.sixteen = new System.Windows.Forms.CheckBox();
+            this.twenty = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Check the desired length of new password\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Your new password:";
+            // 
+            // Password
+            // 
+            this.Password.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Password.Location = new System.Drawing.Point(228, 273);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(293, 23);
+            this.Password.TabIndex = 7;
+            // 
+            // Passwordbtn
+            // 
+            this.Passwordbtn.Location = new System.Drawing.Point(228, 172);
+            this.Passwordbtn.Name = "Passwordbtn";
+            this.Passwordbtn.Size = new System.Drawing.Size(93, 48);
+            this.Passwordbtn.TabIndex = 8;
+            this.Passwordbtn.Text = "Get New Password";
+            this.Passwordbtn.UseVisualStyleBackColor = true;
+            this.Passwordbtn.Click += new System.EventHandler(this.Passwordbtn_Click);
+            // 
+            // Clearbtn
+            // 
+            this.Clearbtn.Location = new System.Drawing.Point(170, 326);
+            this.Clearbtn.Name = "Clearbtn";
+            this.Clearbtn.Size = new System.Drawing.Size(75, 23);
+            this.Clearbtn.TabIndex = 9;
+            this.Clearbtn.Text = "Clear";
+            this.Clearbtn.UseVisualStyleBackColor = true;
+            this.Clearbtn.Click += new System.EventHandler(this.Clearbtn_Click);
+            // 
+            // Exitbtn
+            // 
+            this.Exitbtn.Location = new System.Drawing.Point(331, 326);
+            this.Exitbtn.Name = "Exitbtn";
+            this.Exitbtn.Size = new System.Drawing.Size(75, 23);
+            this.Exitbtn.TabIndex = 10;
+            this.Exitbtn.Text = "Exit";
+            this.Exitbtn.UseVisualStyleBackColor = true;
+            this.Exitbtn.Click += new System.EventHandler(this.Exitbtn_Click);
+            // 
+            // eight
+            // 
+            this.eight.AutoSize = true;
+            this.eight.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eight.Location = new System.Drawing.Point(110, 86);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(126, 20);
+            this.eight.TabIndex = 11;
+            this.eight.Text = "8 characters...";
+            this.eight.UseVisualStyleBackColor = true;
+            // 
+            // ten
+            // 
+            this.ten.AutoSize = true;
+            this.ten.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ten.Location = new System.Drawing.Point(110, 131);
+            this.ten.Name = "ten";
+            this.ten.Size = new System.Drawing.Size(134, 20);
+            this.ten.TabIndex = 12;
+            this.ten.Text = "10 characters...";
+            this.ten.UseVisualStyleBackColor = true;
+            // 
+            // sixteen
+            // 
+            this.sixteen.AutoSize = true;
+            this.sixteen.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sixteen.Location = new System.Drawing.Point(290, 86);
+            this.sixteen.Name = "sixteen";
+            this.sixteen.Size = new System.Drawing.Size(134, 20);
+            this.sixteen.TabIndex = 13;
+            this.sixteen.Text = "16 characters...";
+            this.sixteen.UseVisualStyleBackColor = true;
+            // 
+            // twenty
+            // 
+            this.twenty.AutoSize = true;
+            this.twenty.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.twenty.Location = new System.Drawing.Point(290, 131);
+            this.twenty.Name = "twenty";
+            this.twenty.Size = new System.Drawing.Size(134, 20);
+            this.twenty.TabIndex = 14;
+            this.twenty.Text = "20 characters...";
+            this.twenty.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 370);
+            this.Controls.Add(this.twenty);
+            this.Controls.Add(this.sixteen);
+            this.Controls.Add(this.ten);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.Exitbtn);
+            this.Controls.Add(this.Clearbtn);
+            this.Controls.Add(this.Passwordbtn);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Password Generator....";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Button Passwordbtn;
+        private System.Windows.Forms.Button Clearbtn;
+        private System.Windows.Forms.Button Exitbtn;
+        private System.Windows.Forms.CheckBox eight;
+        private System.Windows.Forms.CheckBox ten;
+        private System.Windows.Forms.CheckBox sixteen;
+        private System.Windows.Forms.CheckBox twenty;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _PasswordGenerator_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Clearbtn_Click(object sender, EventArgs e)
+        {
+            // Clear all the text fields.
+            Password.Text = "";
+           
+        }
+
+        private void Exitbtn_Click(object sender, EventArgs e)
+        {
+            // The exit button.
+            this.Close();
+        }
+
+        private void Passwordbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // If the eight checkbox is checked.
+                if (eight.Checked == true)
+                {
+                    // To make sure other check boxes are disabled.
+                    ten.Checked = false;
+                    sixteen.Checked = false;
+                    twenty.Checked = false;
+
+                    // Creating a new password using a random function.
+                    const string str = "abcdefghijklmnopqrstuvwxyz!@#$%^&*(){}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+                    Random r = new Random();
+                    string Eight = new string(str.ToCharArray().OrderBy(s => (r.Next(4) % 2) == 0).ToArray());
+
+                    // Display new password that is eight characters in length.
+                    Password.Text = Eight[15].ToString() + Eight[1].ToString() + Eight[10].ToString() + Eight[23].ToString()
+                        + Eight[20].ToString() + Eight[5].ToString() + Eight[30].ToString() + Eight[40].ToString();
+                }
+
+                // If the sixteen checkbox is checked.
+                if (ten.Checked == true)
+                {
+                    // To make sure other check boxes are disabled.
+                    eight.Checked = false;
+                    sixteen.Checked = false;
+                    twenty.Checked = false;
+
+                    // Creating a new password using a random function.
+                    const string str = "abcdefghijklmnopqrstuvwxyz!@#$%^&*(){}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+                    Random r = new Random();
+                    string Ten = new string(str.ToCharArray().OrderBy(s => (r.Next(4) % 2) == 0).ToArray());
+
+                    // Display new password that is eight characters in length.
+                    Password.Text = Ten[15].ToString() + Ten[1].ToString() + Ten[10].ToString() + Ten[23].ToString()
+                        + Ten[20].ToString() + Ten[5].ToString() + Ten[30].ToString() + Ten[40].ToString()
+                        + Ten[11].ToString() + Ten[13].ToString();
+                }
+
+
+                // If the sixteen checkbox is checked.
+                if (sixteen.Checked == true)
+                {
+                    // To make sure other check boxes are disabled.
+                    eight.Checked = false;
+                    ten.Checked = false;
+                    twenty.Checked = false;
+
+                    // Creating a new password using a random function.
+                    const string str = "abcdefghijklmnopqrstuvwxyz!@#$%^&*(){}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+                    Random r = new Random();
+                    string Sixteen = new string(str.ToCharArray().OrderBy(s => (r.Next(4) % 2) == 0).ToArray());
+
+                    // Display new password that is eight characters in length.
+                    Password.Text = Sixteen[15].ToString() + Sixteen[1].ToString() + Sixteen[10].ToString() + Sixteen[23].ToString()
+                        + Sixteen[20].ToString() + Sixteen[5].ToString() + Sixteen[30].ToString() + Sixteen[40].ToString() 
+                        + Sixteen[11].ToString() + Sixteen[13].ToString() + Sixteen[32].ToString() + Sixteen[33].ToString() + Sixteen[43].ToString()
+                        + Sixteen[41].ToString()+ Sixteen[36].ToString() + Sixteen[34].ToString();
+                }
+
+                // If the sixteen checkbox is checked.
+                if (twenty.Checked == true)
+                {
+                    // To make sure other check boxes are disabled.
+                    eight.Checked = false;
+                    ten.Checked = false;
+                    sixteen.Checked = false;
+
+                    // Creating a new password using a random function.
+                    const string str = "abcdefghijklmnopqrstuvwxyz!@#$%^&*(){}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+                    Random r = new Random();
+                    string Twenty = new string(str.ToCharArray().OrderBy(s => (r.Next(4) % 2) == 0).ToArray());
+
+                    // Display new password that is eight characters in length.
+                    Password.Text = Twenty[15].ToString() + Twenty[1].ToString() + Twenty[10].ToString() + Twenty[23].ToString()
+                        + Twenty[20].ToString() + Twenty[5].ToString() + Twenty[30].ToString() + Twenty[40].ToString()
+                        + Twenty[11].ToString() + Twenty[13].ToString() + Twenty[32].ToString() + Twenty[33].ToString() + Twenty[43].ToString()
+                        + Twenty[41].ToString() + Twenty[36].ToString() + Twenty[34].ToString() + Twenty[25].ToString() + Twenty[21].ToString()
+                        + Twenty[8].ToString() + Twenty[6].ToString();
+                }
+            }
+
+            // In case of error display error message.
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+
+
+        }
+    }
+}
